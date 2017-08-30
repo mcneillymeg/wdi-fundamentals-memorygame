b01849285111dbb5d0b657281b7f2a36e509e3a8
megan mcneilly <meganmcneilly@megans-MacBook-Pro.local>
2017-08-30 14:39:28 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,17 +1,23 @@
 var cards = ['queen', 'queen', 'king', 'king'];
 var cardsInPlay = [];
 
-var cardOne = 'cards' [0];
-cardsInPlay.push('cardOne');
-console.log('User flipped queen')
-
-var cardTwo = cards[3]
-cardsInPlay.push('cardThree');
-console.log('user flipped king');
-
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
-	alert('you found a match');
-} else {
-	alert('sorry, try again');
+var checkForMatch = function () {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+      console.log("You found a match!");
+  } else {
+      console.log("Sorry, try again.");
+  }
 }
 
+var flipCard = function (cardId) {
+	cardsInPlay.push(cards[cardId]);
+
+	console.log('User flipped ' + cards[cardId]);
+
+	if (cardsInPlay.length === 2); {
+		checkForMatch();
+	}
+}
+
+flipCard([0]);
+flipCard([2]);
